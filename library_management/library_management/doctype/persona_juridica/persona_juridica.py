d8d771a63bebe4b82a1763595ae7334820cb0494
Danny Molina Morales <dmolina@iguanait.com>
2022-12-30 14:13:26 -0500

    fix: repair doctype PJ

--- library_management/library_management/doctype/persona_juridica/persona_juridica.py
+++ library_management/library_management/doctype/persona_juridica/persona_juridica.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.utils.nestedset import NestedSet
+from frappe.model.document import Document
 
 
-class PersonaJuridica(NestedSet):
-	pass
+class PersonaJuridica(Document):
+    pass
